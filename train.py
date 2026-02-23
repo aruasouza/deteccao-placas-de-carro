800aa38879b9df1335558e84bde85efde4d9c3af
Arua Souza <arua.viso@gmail.com>
2026-02-23 09:59:47 -0300

    update

--- train.py
+++ train.py
@@ -1,6 +1,4 @@
 from ultralytics import YOLO
-import shutil
-import os
 
 EPOCHS = 100
 
@@ -12,7 +10,3 @@ if __name__ == '__main__':
         batch=-1,
         device='cuda',
     )
-    last_train = sorted(os.listdir('runs/pose/train/weights'))[-1]
-    model = YOLO(f'runs/pose/{last_train}/weights/best.pt')
-    model.export(format='onnx')
-    shutil.move(f'runs/pose/{last_train}/weights/best.onnx', 'onnx_models/best.onnx')
