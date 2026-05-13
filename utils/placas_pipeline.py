ad1c72c907924eadf7c2910dcd2cfba5262ffe0a
Arua Souza <arua.viso@gmail.com>
2026-05-13 10:17:28 -0300

    fix

--- utils/placas_pipeline.py
+++ utils/placas_pipeline.py
@@ -4,12 +4,12 @@ from utils.inferencia_ocr import PlateOCR
 
 env = os.getenv('ENV', 'DEV')
 
-if env == 'PROD':
-    ocr_model_me = PlateOCR('rknn_models/plate_ocr_me.rknn')
-    ocr_model_br = PlateOCR('rknn_models/plate_ocr_br.rknn')
-else:
-    ocr_model_me = PlateOCR('onnx_models/plate_ocr_me.onnx')
-    ocr_model_br = PlateOCR('onnx_models/plate_ocr_br.onnx')
+# if env == 'PROD':
+#     ocr_model_me = PlateOCR('rknn_models/plate_ocr_me.rknn')
+#     ocr_model_br = PlateOCR('rknn_models/plate_ocr_br.rknn')
+# else:
+ocr_model_me = PlateOCR('onnx_models/plate_ocr_me.onnx')
+ocr_model_br = PlateOCR('onnx_models/plate_ocr_br.onnx')
 
 def inferencia_me(img):
     result = {'tempo':{'tempo_extracao':0}}
